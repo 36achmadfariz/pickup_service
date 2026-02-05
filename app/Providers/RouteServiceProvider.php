21895cf069cb8f9a0dca1325d1189cd7c4b7568d
Achmad Fariz Supriatna - NIM 714252007 <36achmadfariz@gmail.com>
2026-02-05 09:54:35 +0700

    Prepare project for Railway deployment

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/pickup_requests';
+
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
